efc150e1b11a371262bd3111101ec68a869bf69d
Ele Palmer <ecpalmer21@gmail.com>
2016-06-30 16:14:01 -0400

    Years populate dropdown

--- carData.js
+++ carData.js
@@ -2,16 +2,48 @@
 
     var carData = function ($http) {
 
+         getYears = function () {
+            return $http.get('http://epalmer-api.azurewebsites.net/api/Car/Years');
+            // .then(function (response) {
+            //     return response.data;
+            // });
+        };
+
+        getMakes = function (year) {
+            return $http.get('http://epalmer-api.azurewebsites.net/api/Car/' + year + '/Makes')
+            .then(function (response) {
+                return response.data;
+            });
+        };
+
+        getModels = function (year, make) {
+            return $http.get('http://epalmer-api.azurewebsites.net/api/Car/' + year + '/' + make + '/Models')
+            .then(function (response) {
+                return response.data;
+            });
+        };
+
+        getTrims = function (year, make, model) {
+            return $http.get('http://epalmer-api.azurewebsites.net/api/Car/' + year + '/' + make +  '/' + model + '/Trims')
+            .then(function (response) {
+                return response.data;
+            });
+        };
+
         getCarData = function (year, make, model, trim) {
             return $http.get('http://epalmer-api.azurewebsites.net/api/Car/getCar?year=' + year + '&make='
                 + make + '&Model=' + model + '&Trim=' + trim)
+            //2012, Honda, Accord, Sedan SE
             .then(function (response) {
-                console.log(response);
                 return response.data;
             });
         };
 
         return {
+            getYears: getYears,
+            getMakes, getMakes,
+            getModels, getModels,
+            getTrims: getTrims,
             getCarData: getCarData
         };
     };
--- mainController.js
+++ mainController.js
@@ -1,8 +1,47 @@
 (function() {
 var app = angular.module('appCar');
 
-var mainController = function ($scope) {
-  $scope.title = "Car Finder";
+var mainController = function ($scope, $http, carData, $routeParams) {
+      $scope.years = [];
+      $scope.makes = [];
+      $scope.models = [];
+      $scope.trims = [];
+      $scope.selectedYear = $scope.years[0];
+      $scope.selectedMake = null;
+      $scope.selectedModel = null;
+      $scope.selectedTrim = null;
+      $scope.showYears = true;
+      $scope.showMakes = false;
+      $scope.showModels = false;
+      $scope.showTrims = false;
+
+      // $scope.appendData = function(element, array) {
+      //   //add select list yearsDropdown
+      //   console.log($scope.years + " " + $scope.showYears);
+      //   var itemDropdown = document.getElementById(element);
+      //   for(item in array) {
+      //     itemDropdown.add(new Option( array[item] ));
+      //   }
+      // };
+
+      $scope.getYears = function () {
+        carData.getYears().success(function (result) {
+            $scope.years = result;
+            $scope.showYears = true;   
+            console.log($scope.showYears); 
+            console.log($scope.years);
+            console.log($scope.years[0]);
+        });
+      };
+
+       $scope.getMakes = function (year) {
+        carData.getMakes(year).then(function (data) {
+            $scope.makes = data;
+            $scope.showMakes = true;    
+        });
+      };
+
+      $scope.getYears();
 }
 
 app.controller('mainController', mainController);
